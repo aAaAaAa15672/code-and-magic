9a0a3ce965adbec89ff9e76168d2c107dd1e1ae6
aAaAaAa15672 <onlyforpc10@gmail.com>
2021-02-16 19:40:47 +0500

    канвас

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,21 @@
-const renderStatistics = function(ctx, names, times){
+window.renderStatistics = function(ctx, names, times) {
+  // debugger;
 
+//   const canvasElement = document.getElementById ('canvas');
+//   const ctx = canvasElement.getContext('2d');
+
+  ctx.strokeStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.strokeRect (110, 20, 270, 420);
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect (100, 10, 270, 420);
+
+
+
+  /*ctx.clearRect(100, 10, 270, 420);*/
+
+  ctx.font = '16px PT Mono';
+  fillStyle = 'black';
+  ctx.fillText = ('Ура вы победили!', 120, 30)
+  // ctx.fillText = 'Список результатов:';
 };
